5f4dd941684dacf261435e5b705fee4543db9ed3
miclea-mario <miclea.alexandru.mario@gmail.com>
2025-07-10 20:06:30 +0300

    Remove search component from HeroLayout and clean up header

--- components/HeroLayout.jsx
+++ components/HeroLayout.jsx
@@ -1,40 +1,10 @@
 import { Header, Link } from '@components';
-import { MapPin, SearchIcon } from 'lucide-react';
 import { Button } from './ui/button';
-
-const Search = () => {
-  return (
-    <div className="bg-white/20 backdrop-blur-md rounded-full p-2 flex items-center space-x-4 h-[50px]">
-      <div className="max-w-[300px] flex items-center space-x-4">
-        <div className="flex items-center gap-2">
-          <SearchIcon className="text-white/50" />
-          <input
-            className="bg-transparent outline-none text-white/50 text-sm"
-            placeholder="Search job..."
-          ></input>
-        </div>
-        <hr className="h-6 w-[1px] bg-white/20"></hr>
-        <div className="flex items-center gap-2">
-          <MapPin className="text-white/50" />
-          <input
-            className="bg-transparent outline-none text-white/50 text-sm"
-            placeholder="Location"
-          ></input>
-        </div>
-      </div>
-      <button className="rounded-full bg-none text-sm bg-white h-full px-4 hover:bg-primary">
-        Search
-      </button>
-    </div>
-  );
-};
 
 const HeroLayout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col cover">
-      <Header className="absolute top-0 z-20 w-full">
-        <Search />
-      </Header>
+      <Header className="absolute top-0 z-20 w-full" />
 
       {/* Hero Section */}
       <div className="relative min-h-screen flex items-center">
