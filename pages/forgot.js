6bceacdbdcc7690311040e7774b07e6aef8f2774
miclea-mario <miclea.alexandru.mario@gmail.com>
2025-07-13 13:25:09 +0300

    Replace anchor tag with paragraph for back to login link in forgot password page

--- pages/forgot.js
+++ pages/forgot.js
@@ -6,7 +6,7 @@ const Page = () => {
     <main className="cover flex min-h-screen flex-col items-center justify-center px-4 py-8">
       <div className="my-8 flex w-full max-w-xl flex-col rounded-lg bg-white px-4 py-8 lg:px-12">
         <Link href="/login">
-          <a className="hover:underline">← back to login</a>
+          <p className="hover:underline">← back to login</p>
         </Link>
         <h2 className="mb-4 text-2xl font-bold">Password recovery</h2>
         <ForgotForm />
